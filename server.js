4f101155cfae0b26445faf1605fe5f35eae2faef
elhamcr36-droid <elhamcr36@gmail.com>
2026-03-14 23:48:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const { Pool } = require("pg");
+
 const app = express();
+app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Poultry Enterprise API running 🐔");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Database error");
+  }
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port " + PORT);
 });
