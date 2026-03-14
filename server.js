9f19f61c235b444d9d1d9a7f81858a65dba0a67b
elhamcr36-droid <elhamcr36@gmail.com>
2026-03-14 23:54:09 +0700

    Update server.js
    connect postgres database

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ app.use(express.json());
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: { rejectUnauthorized: false }
 });
 
 app.get("/", (req, res) => {
@@ -20,12 +18,12 @@ app.get("/test-db", async (req, res) => {
     const result = await pool.query("SELECT NOW()");
     res.json(result.rows);
   } catch (err) {
-    console.error(err);
     res.status(500).send("Database error");
   }
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
